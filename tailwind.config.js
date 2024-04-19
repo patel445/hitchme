f956bad705b6d4f9259b96e36fa2c6428186d64a
Jay Patel <jaypatel@Jays-MacBook-Pro-2.local>
2024-04-19 13:43:44 -0500

    removed unused code

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,6 @@ module.exports = {
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
   ],
-  app: {
-    head: {
-      link: [],
-    },
-  },
   plugins: [require("@tailwindcss/typography")],
   theme: {
     extend: {
